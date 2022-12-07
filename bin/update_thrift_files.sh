6e7841e1c73ad24db86914751d1f9a9cd633517a
Geidson Benicio Coelho <geidsonc@gmail.com>
2022-12-07 18:26:27 -0300

    Atualiza script de compilação

--- bin/update_thrift_files.sh
+++ bin/update_thrift_files.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
 git clone https://github.com/laboratoriobridge/esusab-integracao storage/esusab || true
 
 layouts=(
 	'layout-camada-transport'
 	'layout-ras'
+	'layout-cidadao'
 )
 
 mapped_directories=(
 	'transport'
 	'cds'
+	'cidadao'
 )
 
 size=$((${#layouts[@]}))
@@ -18,6 +19,11 @@ for (( i=0; i<$size; i++ ))
 do
 	for FILE in storage/esusab/thrift/${layouts[$i]}/thrift/*.thrift
 	do
-		thrift -o src/${mapped_directories[$i]} --gen php $FILE
+		mkdir -p src/${mapped_directories[$i]}
+		# thrift -o src/bu/${mapped_directories[$i]} --gen php $FILE
+		# docker run -v "$PWD:$(pwd)" thrift:0.9.3 thrift -r -o "$(pwd)/src/${mapped_directories[$i]}" --gen php:oop -v $(pwd)/$FILE
+		docker run -v "$PWD:$(pwd)" thrift:0.14.1 thrift -o "$(pwd)/src/${mapped_directories[$i]}" --gen php -v $(pwd)/$FILE
 	done
 done
+
+rm -f storage/esusab -R
