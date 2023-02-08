b11d11dc59ec0244523cc3da808c0bc8b9640d39
Geidson Benicio Coelho <geidsonc@gmail.com>
2023-02-08 14:53:47 -0300

    Ajusta script de build

--- bin/update_thrift_files.sh
+++ bin/update_thrift_files.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 git clone https://github.com/laboratoriobridge/esusab-integracao storage/esusab || true
 
+if ! grep "namespace php br.gov.saude.esus.thrift.definition.cidadao" storage/esusab/thrift/layout-cidadao/thrift/cidadao_transport.thrift ; then
+	sed -i '1s/^/namespace php br.gov.saude.esus.thrift.definition.cidadao\n/' storage/esusab/thrift/layout-cidadao/thrift/cidadao_transport.thrift
+fi
+
 layouts=(
 	'layout-camada-transport'
 	'layout-ras'
@@ -20,7 +24,7 @@ do
 	for FILE in storage/esusab/thrift/${layouts[$i]}/thrift/*.thrift
 	do
 		mkdir -p src/${mapped_directories[$i]}
-		docker run -v "$PWD:$(pwd)" thrift:0.14.1 thrift -o "$(pwd)/src/${mapped_directories[$i]}" --gen php -v $(pwd)/$FILE
+		docker run -v "$PWD:$(pwd)" sysvale-thrift:0.17.0 thrift -o "$(pwd)/src/${mapped_directories[$i]}" --gen php -v $(pwd)/$FILE
 	done
 done
 
